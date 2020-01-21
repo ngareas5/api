3fc0f01bded96db144bfff8ff94eb0c887fe5ef7
ngareas5 <naren.gaindhar92@gmail.com>
2020-01-21 13:02:00 +0530

    added error message

--- app/controllers/v1/tokens_controller.rb
+++ app/controllers/v1/tokens_controller.rb
@@ -1,25 +1,21 @@
 class  V1::TokensController < ApplicationController
 	def create
-     user1=User1.find_by(username: params[:username])
-        
+    user1=User1.find_by(username: params[:username]) 
       if user1&.authenticate(params[:password])
-         
-      	render json:{
-
-          jwt: encode_token({id: user1&.id, username: user1&.username})
-      }
-
+        if user1.id.present? && user1.username.present? 
+      	  render json:{ jwt: encode_token({id: user1&.id, username: user1&.username})}
+      	else
+      	  render json: {message: "user not present"}
+        end
        else
-      	head :not_found
-        
-       end
-        
+      	head :not_found 
+       end  
      end
         private
-          
+         
           def encode_token(payload ={})
-
-          exp=	24.hours.from_now
+          	#debugger
+            exp=24.hours.from_now
           payload[:exp]=exp.to_i
           JWT.encode(payload, Rails.application.secrets.secret_key_base)
         end	
