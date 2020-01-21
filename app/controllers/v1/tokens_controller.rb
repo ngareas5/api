15462d3ba44ddd966e3a979e272c538613310127
ngareas5 <naren.gaindhar92@gmail.com>
2020-01-21 15:45:22 +0530

    add hs25

--- app/controllers/v1/tokens_controller.rb
+++ app/controllers/v1/tokens_controller.rb
@@ -3,12 +3,9 @@ class  V1::TokensController < ApplicationController
 		tokens = []
 		user1=User1.find_by(username: params[:username]) 
 		if user1&.authenticate(params[:password])
-			if user1.id.present? && user1.username.present? 
-				tokens = {jwt: encode_token(id: user1&.id, username: user1&.username)}
-				render json:{tokens: tokens}
-			else
-				render json: {message: "user not present"}
-			end
+			
+				   render json: {jwt: encode_token(id: user1&.id, username: user1&.username, exp: (24.hours.from_now).to_i)}
+			
 		else
 			head :not_found 
 		end  
@@ -16,11 +13,11 @@ class  V1::TokensController < ApplicationController
 
 	private
 	 
-		def encode_token(payload)
+		def encode_token(payload={})
 			
-			payload ||= {}
-			exp = 24.hours.from_now
-			payload[:exp] = exp.to_i 
-			JWT.encode(payload, Rails.application.credentials.secret_key_base)
+			#payload ||= {}
+			#exp = 24.hours.from_now
+			#payload[:exp] = exp.to_i 
+			JWT.encode(payload, Rails.application.secrets.secret_key_base,'HS256')
 		end
 end
