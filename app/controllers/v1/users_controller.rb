946cdd4a24f85fc26be298e83e0b77852f2da767
ngareas5 <naren.gaindhar92@gmail.com>
2020-01-22 17:27:57 +0530

    new user

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,23 +1,33 @@
 class V1::UsersController < ApplicationController
  
-  def index
-  	#debugger
-   users=User1.all
-   user_details = []
-   users.each do |user|
-   	user_details << {id: user.id, name: user.name, username: user.username, email: user.email}
+  
 
-   end
+	
 
-   render json: user_details, status: :ok 
-  end
-  def create
-  	
-  	user=User1.new(params.permit(:username,:password,:password_confirmation,:name,:email))
-  	if user.save
-  		render json: { status: 200, message: 'user created'}
-  	else
-      render json: {status: "error",message: user.errors.full_messages.join(',') }
-    end
+	def index
+		#debugger
+	 users=User1.all
+	 user_details = []
+	 users.each do |user|
+		user_details << {id: user.id, name: user.name, username: user.username, email: user.email}
+
+	 end
+
+	 render json: user_details, status: :ok 
+	end
+	
+	def create
+		#debugger
+		user=User1.new(params.permit(:username,:password,:password_confirmation,:name,:email))
+		if user.save
+			render json: { status: 200, message: 'user created'}
+		else
+			render json: {status: "error",message: user.errors.full_messages.join(',') }
+		end
  end
+
+
+
+
+
 end
